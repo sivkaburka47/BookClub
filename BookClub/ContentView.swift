53cbce2e712e63a6b04b5de9f509af2d6a937e67
Stanislav Deynekin <deinekin.std@gmail.com>
2025-03-15 01:22:02 +0700

    Initial commit

---
+++ BookClub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookClub
+//
+//  Created by Станислав Дейнекин on 15.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
