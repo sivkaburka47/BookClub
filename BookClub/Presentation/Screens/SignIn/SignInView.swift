006d6fe5ce09e6d3a63a44dd61e8c259c2691e9d
Stanislav Deynekin <deinekin.std@gmail.com>
2025-03-15 02:12:12 +0700

    Добавлены иконки, шрифты и цвета.

---
+++ BookClub/Presentation/Screens/SignIn/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  BookClub
+//
+//  Created by Станислав Дейнекин on 15.03.2025.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
