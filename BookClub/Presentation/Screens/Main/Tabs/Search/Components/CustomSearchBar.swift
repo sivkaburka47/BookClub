d2f16bfa5feda22c4cd9a70aafb2379305229c11
Stanislav Deynekin <deinekin.std@gmail.com>
2025-03-17 15:35:54 +0700

    Добавлен SearchBar на экран Search, изменен background экрана. Добавлена модель Author

---
+++ BookClub/Presentation/Screens/Main/Tabs/Search/Components/CustomSearchBar.swift
@@ -0,0 +1,48 @@
+//
+//  CustomSearchBar.swift
+//  BookClub
+//
+//  Created by Станислав Дейнекин on 17.03.2025.
+//
+
+import SwiftUI
+
+struct CustomSearchBar: View {
+    @Binding var text: String
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            if text.isEmpty {
+                Image("Search")
+                    .renderingMode(.template)
+                    .foregroundColor(Color("AccentMedium"))
+                    .frame(width: 20, height: 20)
+            }
+            
+            TextField("", text: $text, prompt: Text("Поиск по книгам")
+                .foregroundColor(Color("AccentMedium")))
+                .font(.custom("VelaSans-Regular", size: 16))
+                .foregroundColor(Color("AccentDark"))
+            
+            if !text.isEmpty {
+                Button(action: {
+                    text = ""
+                }) {
+                    Image("Close")
+                        .renderingMode(.template)
+                        .foregroundColor(Color("AccentDark"))
+                        .frame(width: 24, height: 24)
+                }
+            }
+        }
+        .padding(.vertical, 7)
+        .padding(.horizontal, 16)
+        .frame(height: 44)
+        .background(Color("White"))
+        .cornerRadius(12)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color("AccentMedium"), lineWidth: 1)
+        )
+    }
+}
--- BookClub/Presentation/Screens/Main/Tabs/Search/SearchView.swift
+++ BookClub/Presentation/Screens/Main/Tabs/Search/SearchView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText: String = ""
+    
+    let genres = [
+        "Классика", "Фэнтези", "Фантастика", "Детектив",
+        "Триллер", "Исторический роман", "Любовный роман", "Приключения",
+        "Поэзия", "Биография", "Для подростков", "Для детей"
+    ]
+    
+    @State private var recentRequests = ["Android", "iOS", "Windows", "Linux", "MacOS"]
+    
+    @State private var authors = [
+        Author(image: "book", name: "Братья Стругацкие"),
+        Author(image: "book", name: "Дэн Браун"),
+        Author(image: "book", name: "Федор Достоевский"),
+        Author(image: "book", name: "Эйнштейн"),
+        Author(image: "book", name: "Лев Толстой")
+    ]
+    
     var body: some View {
         ZStack {
-            AngularGradient(
-                gradient: Gradient(colors: [.blue, .yellow, .green, .blue, .purple]),
-                center: .center
-            )
-            .ignoresSafeArea()
+            Color("Background")
+                .ignoresSafeArea()
             
-            Text("Поиск")
-                .font(.largeTitle)
-                .foregroundColor(.white)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 24) {
+                    CustomSearchBar(text: $searchText)
+                    
+
+                    
+                    Spacer().frame(height: 100)
+                }
+                .padding(.horizontal, 16)
+            }
         }
     }
+}
+
+#Preview {
+    SearchView()
 }
