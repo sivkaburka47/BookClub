2241661aae48098a5b18816d96b2a625f3de1eb4
Stanislav Deynekin <deinekin.std@gmail.com>
2025-03-17 22:25:24 +0700

    Добавлена секция "Избранные книги" на экран "Закладки"

--- BookClub/Presentation/Screens/Main/Tabs/Bookmarks/BookmarksView.swift
+++ BookClub/Presentation/Screens/Main/Tabs/Bookmarks/BookmarksView.swift
@@ -16,15 +16,27 @@ struct BookmarksView: View {
         chapters: ["Пролог", "Глава 1", "Глава 2", "Глава 3"]
     )
     
+    @State private var books = [
+        BookCard(image: "book", title: "Пикник на обочине", author: "Братья Стругацкие"),
+        BookCard(image: "book", title: "Код да Винчи", author: "Дэн Браун"),
+        BookCard(image: "book", title: "Преступление и наказание", author: "Федор Достоевский"),
+        BookCard(image: "book", title: "Мир как он есть", author: "Альберт Эйнштейн"),
+        BookCard(image: "book", title: "Война и мир", author: "Лев Толстой")
+    ]
+    
     var body: some View {
         ZStack {
             Color("Background")
                 .ignoresSafeArea()
             
             ScrollView {
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 24) {
                     ScreenTitle(text: "Закладки")
                     CurrentReadingSection(book: book)
+                    VStack(alignment: .leading, spacing: 8) {
+                        SectionTitle(text: "Избранные книги")
+                        BookListView(books: books)
+                    }
                     Spacer().frame(height: 100)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
