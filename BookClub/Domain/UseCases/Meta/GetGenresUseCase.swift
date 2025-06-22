f9a27dc96aa4887b1b1d2b417ae05044dd61f0f9
Stanislav Deynekin <164678325+sivkaburka47@users.noreply.github.com>
2025-06-23 02:09:02 +0700

    Добавлены usecase для metaRepository

--- BookClub/Domain/UseCases/Meta/GetGenresUseCase.swift
+++ BookClub/Domain/UseCases/Meta/GetGenresUseCase.swift
@@ -5,3 +5,28 @@
 //  Created by Станислав Дейнекин on 22.06.2025.
 //
 
+protocol GetGenresUseCase {
+    func execute() async throws -> [Genre]
+}
+
+final class GetGenresUseCaseImpl: GetGenresUseCase {
+    private let repository: MetaRepository
+
+    init(repository: MetaRepository) {
+        self.repository = repository
+    }
+
+    static func create() -> GetGenresUseCaseImpl {
+        let httpClient = AlamofireHTTPClient()
+        let repository = MetaRepositoryImpl(httpClient: httpClient)
+        return GetGenresUseCaseImpl(repository: repository)
+    }
+
+    func execute() async throws -> [Genre] {
+        do {
+            return try await repository.getGenres()
+        } catch {
+            throw error
+        }
+    }
+}
