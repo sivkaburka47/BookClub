a9e4408c6fd29d1b3de375eeb35c5c379909ce41
Stanislav Deynekin <164678325+sivkaburka47@users.noreply.github.com>
2025-06-23 02:37:44 +0700

    Добавлены usecase для quoteRepository

--- BookClub/Domain/UseCases/Quote/CreateQuoteUseCase.swift
+++ BookClub/Domain/UseCases/Quote/CreateQuoteUseCase.swift
@@ -5,3 +5,28 @@
 //  Created by Станислав Дейнекин on 22.06.2025.
 //
 
+protocol CreateQuoteUseCase {
+    func execute(quote: Quote) async throws
+}
+
+final class CreateQuoteUseCaseImpl: CreateQuoteUseCase {
+    private let repository: QuoteRepository
+
+    init(repository: QuoteRepository) {
+        self.repository = repository
+    }
+
+    static func create() -> CreateQuoteUseCaseImpl {
+        let httpClient = AlamofireHTTPClient()
+        let repository = QuoteRepositoryImpl(httpClient: httpClient)
+        return CreateQuoteUseCaseImpl(repository: repository)
+    }
+
+    func execute(quote: Quote) async throws {
+        do {
+            try await repository.createQuote(quote: quote)
+        } catch {
+            throw error
+        }
+    }
+}
--- BookClub/Domain/UseCases/Quote/GetQuotesUseCase.swift
+++ BookClub/Domain/UseCases/Quote/GetQuotesUseCase.swift
@@ -5,3 +5,28 @@
 //  Created by Станислав Дейнекин on 22.06.2025.
 //
 
+protocol GetQuotesUseCase {
+    func execute() async throws -> [Quote]
+}
+
+final class GetQuotesUseCaseImpl: GetQuotesUseCase {
+    private let repository: QuoteRepository
+
+    init(repository: QuoteRepository) {
+        self.repository = repository
+    }
+
+    static func create() -> GetQuotesUseCaseImpl {
+        let httpClient = AlamofireHTTPClient()
+        let repository = QuoteRepositoryImpl(httpClient: httpClient)
+        return GetQuotesUseCaseImpl(repository: repository)
+    }
+
+    func execute() async throws -> [Quote]{
+        do {
+            return try await repository.getQuotes()
+        } catch {
+            throw error
+        }
+    }
+}
