16f48050baf925d2893f26728704e8ff865165fb
Stanislav Deynekin <164678325+sivkaburka47@users.noreply.github.com>
2025-06-23 02:33:10 +0700

    Добавлены usecase для progressRepository

--- BookClub/Domain/UseCases/Progress/GetProgressUseCase.swift
+++ BookClub/Domain/UseCases/Progress/GetProgressUseCase.swift
@@ -5,3 +5,28 @@
 //  Created by Станислав Дейнекин on 22.06.2025.
 //
 
+protocol GetProgressUseCase {
+    func execute() async throws -> [Progress]
+}
+
+final class GetProgressUseCaseImpl: GetProgressUseCase {
+    private let repository: ProgressRepository
+
+    init(repository: ProgressRepository) {
+        self.repository = repository
+    }
+
+    static func create() -> GetProgressUseCaseImpl {
+        let httpClient = AlamofireHTTPClient()
+        let repository = ProgressRepositoryImpl(httpClient: httpClient)
+        return GetProgressUseCaseImpl(repository: repository)
+    }
+
+    func execute() async throws -> [Progress] {
+        do {
+            return try await repository.getProgress()
+        } catch {
+            throw error
+        }
+    }
+}
--- BookClub/Domain/UseCases/Progress/SaveProgressUseCase.swift
+++ BookClub/Domain/UseCases/Progress/SaveProgressUseCase.swift
@@ -5,3 +5,28 @@
 //  Created by Станислав Дейнекин on 22.06.2025.
 //
 
+protocol SaveProgressUseCase {
+    func execute(progress: Progress) async throws
+}
+
+final class SaveProgressUseCaseImpl: SaveProgressUseCase {
+    private let repository: ProgressRepository
+
+    init(repository: ProgressRepository) {
+        self.repository = repository
+    }
+
+    static func create() -> SaveProgressUseCaseImpl {
+        let httpClient = AlamofireHTTPClient()
+        let repository = ProgressRepositoryImpl(httpClient: httpClient)
+        return SaveProgressUseCaseImpl(repository: repository)
+    }
+
+    func execute(progress: Progress) async throws {
+        do {
+            try await repository.saveProgress(progress: progress)
+        } catch {
+            throw error
+        }
+    }
+}
